7396689f2ca42d91a2911aebac5a700afaefe9d5
nyano <nayan100@hotmail.co.uk>
2021-04-27 14:49:13 +0100

    Initial commit of the project and Exercise 1.6 completed.

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const updateButton = (name) => {
+    if (name === "good") {
+      setGood(good + 1);
+    } else if (name === "neutral") {
+      setNeutral(neutral + 1);
+    } else if (name === "bad") {
+      setBad(bad + 1);
+    }
+  };
+
+  return (
+    <div>
+      <Title name={"give feedback"}/> 
+      <Button 
+        name={"good"}
+        updateButton={updateButton}
+      />
+      <Button 
+        name={"neutral"}
+        updateButton={updateButton}
+      />
+      <Button 
+        name={"bad"}
+        updateButton={updateButton}
+      />
+      <Title name={"statistics"}/>
+      <StatField 
+        good = {good}
+        neutral = {neutral}
+        bad = {bad}
+      />
+    </div>
+  )
+}
+
+const Title = ( {name} ) => {
+  return (
+    <h2>{name}</h2>
+  )
+};
+
+const Button = ( {name, updateButton}) => {
+  return (
+    <button onClick={() => updateButton(name)}>{name}</button>
+  )
+};
+
+const StatField = ( {good, neutral, bad} ) => {
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
