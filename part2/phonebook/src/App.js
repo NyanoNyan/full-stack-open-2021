44e1dc8ff00c3aabc800420263ebfdd5a56ad3a3
nyano <nayan100@hotmail.co.uk>
2021-05-04 15:43:57 +0100

    Exercise 2.7 updated completion, reversal of ternary operator

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,10 +5,15 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone ] = useState('')
 
   const inputChange = (event) => {
     setNewName(event.target.value);
   };
+  
+  const phoneChange = (event) => {
+    setNewPhone(event.target.value);
+  }
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -18,7 +23,7 @@ const App = () => {
 
     // Checks for duplication using conditional (ternary) operator
     persons.includes(newName)
-    ? setPersons(persons.concat(noteObj)) : alert(`${newName} is already added to phonebook`);
+    ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(noteObj));
 
     setNewName('');
   };
@@ -30,10 +35,12 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={inputChange}/>
         </div>
+        <div>number: <input value={newPhone} onChange={phoneChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       {persons.map(person => 
         <div key={person.name}>
