1e3e205e061bfd81ea8325f17af9d8de7c9f1678
nyano <nayan100@hotmail.co.uk>
2021-05-04 20:12:50 +0100

    Exercise 2.9 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
-
+  const [ searchItem, setSearchItem ] = useState('');
+  
   const inputChange = (event) => {
     setNewName(event.target.value);
   };
   
   const phoneChange = (event) => {
     setNewPhone(event.target.value);
+  }
+
+  const searchChange = (event) => {
+    setSearchItem(event.target.value);
   }
 
   const addPerson = (event) => {
@@ -30,10 +38,20 @@ const App = () => {
     setNewPhone('');
   };
 
+  // Search functionality based on name (case insensitive using ternary operator)
+  let namesToShow = searchItem.length === 0
+  ? persons
+  : persons.filter(item => item.name.toLowerCase().includes(searchItem.toLowerCase()));
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <label>filter shown with: </label>
+        <input value={searchItem} onChange={searchChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={inputChange}/>
@@ -45,13 +63,14 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(person => 
+      {namesToShow.map(person => 
         <div key={person.name}>
           {person.name}
           {' '}
           {person.number}
         </div>
         )}
+        
     </div>
   )
 }
