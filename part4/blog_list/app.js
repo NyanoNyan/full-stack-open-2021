d56a8e60b92f8b81a845621c391b7228707ebd23
nyano <nayan100@hotmail.co.uk>
2021-05-17 08:59:56 +0100

    Exercise 4.2 completed

---
+++ part4/blog_list/app.js
@@ -0,0 +1,22 @@
+const config = require('./utils/config');
+const express = require('express');
+const cors = require('cors');
+const notesRouter = require('./controllers/blog');
+const app = express();
+const logger = require('./utils/logger')
+const mongoose = require('mongoose');
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(results => {
+        logger.info('connected to MongoDB');
+    })
+    .catch(error => {
+        logger.error('error connection to MONGODB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', notesRouter);
+
+module.exports = app
