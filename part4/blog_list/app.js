8c3d133285e58893ee15940de871e756d1d3aba1
nyano <nayan100@hotmail.co.uk>
2021-05-24 07:53:59 +0100

    Exercise 4.20 completed

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -20,11 +20,13 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor);
 
 app.use('/api/blogs', notesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
 app.use(middleware.errorHandler);
+
 
 module.exports = app
--- part4/blog_list/controllers/blog.js
+++ part4/blog_list/controllers/blog.js
@@ -10,19 +10,20 @@ blogRouter.get('/', async(request, response) => {
   response.json(blogs)
 });
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')){
-    return authorization.substring(7);
-  }
-  return null;
-}
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization');
+//   if (authorization && authorization.toLowerCase().startsWith('bearer ')){
+//     return authorization.substring(7);
+//   }
+//   return null;
+// }
   
 blogRouter.post('/', async(request, response) => {
   const body = request.body;
-  const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  // const token = getTokenFrom(request);
+  console.log(request.token)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' });
   };
   const user = await User.findById(decodedToken.id);
