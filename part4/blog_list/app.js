ddbbb47b572d0b8f389607c9a684c7f11100aaa1
nyano <nayan100@hotmail.co.uk>
2021-06-07 07:21:37 +0100

    Exercise 5.17 completed

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -26,7 +26,11 @@ app.use('/api/blogs', notesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
-app.use(middleware.errorHandler);
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing');
+    app.use('/api/testing', testingRouter);
+}
 
+app.use(middleware.errorHandler);
 
 module.exports = app
